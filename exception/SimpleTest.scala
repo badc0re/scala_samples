b6a209e135b51721c8c3ea7b47d3e09ec9dabec4
badc0re <dame@vionlabs.com>
2016-08-10 12:03:32 +0200

    Organise

--- exception/SimpleTest.scala
+++
@@ -1,7 +0,0 @@
-object Simple{
-  def main(args: Array[String]){
-    val sc = new java.util.Scanner(System.in)
-    print(sc.nextLine())
-
-  }
-}
--- types_sample/TypesSample.scala
+++
@@ -1,8 +0,0 @@
-
-
-trait Cache[K, V]{
-  def get(key: K): V
-  def pug(key: K, value: V)
-  def delete(key: K)
-
-}
